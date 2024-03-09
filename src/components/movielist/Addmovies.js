4fb2f3eb9b8ce0760b6191b18c556bc75414278c
Nandhini S <sainandhu1998@gmail.com>
2024-03-09 17:37:24 +0530

    code format changed

--- src/components/movielist/Addmovies.js
+++ src/components/movielist/Addmovies.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, {useState} from "react";
 import {useNavigate} from "react-router-dom"
 
 
@@ -7,99 +7,109 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 
 
+const Addmovieform = ({user, parentfn}) => {
+
+    const navigate = useNavigate();
+
+    const newList = [...user]
+
+    const [data, setData] = useState(newList);
+
+    const [moviename, setName] = useState("");
+
+    const [rating, setRating] = useState("");
+
+    const [summary, setSummary] = useState("");
+
+    const [poster, setPoster] = useState("");
+
+    const [trailer, setTrailer] = useState("");
+
+    function addMovie(e) {
 
 
-const Addmovieform =({user,parentfn})=>{
+        e.preventDefault();
+        //  console.log("adding the movie data");
+        //  console.log(moviename);
+        //  console.log(rating);
+        //  console.log(summary);
+        //  console.log(poster);
+        const newObj = {
+            moviename: moviename,
+            rating: rating,
+            summary: summary,
+            poster: poster,
+            trailer: trailer
 
-  const navigate = useNavigate();
+        }
 
-   const newList = [...user]
+        var temparr = [...data, newObj]
+        parentfn(temparr)
+        setData(temparr);
 
-  const[data,setData] = useState(newList);
 
-  const[moviename,setName] = useState("");
+        setName(" ");
+        setRating(" ");
+        setSummary("");
+        setPoster("")
+        setTrailer("")
+        navigate("/Movies");
 
-   const[rating,setRating] = useState("");
-
-   const[summary,setSummary] = useState("");
-
-   const[poster,setPoster] = useState("");
-
-   const[trailer,setTrailer] = useState("");
-
-   function addMovie(e){
-
-    
-    e.preventDefault();
-    //  console.log("adding the movie data");
-    //  console.log(moviename);
-    //  console.log(rating);
-    //  console.log(summary);
-    //  console.log(poster);
-    const newObj={
-      moviename:moviename,
-      rating:rating,
-      summary:summary,
-      poster:poster,
-      trailer:trailer
-    
     }
-   
-    var temparr=[...data,newObj]
-    parentfn(temparr)
-    setData(temparr);
 
 
-    setName(" ");
-    setRating(" ");
-    setSummary("");
-    setPoster("")
-    setTrailer("")
-    navigate("/Movies");
-    
-   }
+    function addName(e) {
 
 
+        setName(e.target.value)
 
-   function addName(e){
+    }
 
-    
-    setName(e.target.value)
+    //console.log("test changes");
 
-   }
+    //  useNavigate("/Movies");
 
-   //console.log("test changes");
 
-  //  useNavigate("/Movies");
-   
+    return (
+        <div>
+            <form>
+                <TextField id="standard-basic" label="MovieName" variant="standard" type="text" name="moviename"
+                           value={moviename} onChange={addName}/>
+                {/* MovieName:<input type="text" name="moviename" value={moviename} onChange={addName}></input> */}
+                <br></br>
+                <TextField id="standard-basic" label="Rating" variant="standard" type="number" step="any" name="rating"
+                           value={rating} onChange={(e) => {
+                    setRating(e.target.value)
+                }}/>
+                {/* Rating:<input type="number" step="any" name="rating" value={rating} onChange={(e)=>{setRating(e.target.value)}}></input> */}
+                <br></br>
+                <TextField id="standard-basic" label="Summary" variant="standard" type="text" name="summary"
+                           value={summary} onChange={(e) => {
+                    setSummary(e.target.value)
+                }}/>
+                {/* Summary:<input type="text" name="summary" value={summary} onChange={(e)=>{setSummary(e.target.value)}}></input> */}
+                <br></br>
+                <TextField id="standard-basic" label="PosterLink" variant="standard" type="text" name="poster"
+                           value={poster} onChange={(e) => {
+                    setPoster(e.target.value)
+                }}/>
+                {/* PosterLink:<input type="text" name="poster" value={poster} onChange={(e)=>{setPoster(e.target.value)}}></input> */}
+                <br></br>
+                <TextField id="standard-basic" label="TrailerLink" variant="standard" type="text" name="trailer"
+                           value={trailer} onChange={(e) => {
+                    setTrailer(e.target.value)
+                }}/>
+                <br></br>
+                {/* <button onClick={addMovie}>Submit</button> */}
+                <br></br>
 
-  return (
-    <div>
-      <form>
-        <TextField id="standard-basic" label="MovieName" variant="standard" type="text" name="moviename" value={moviename} onChange={addName} />
-        {/* MovieName:<input type="text" name="moviename" value={moviename} onChange={addName}></input> */}
-        <br></br>
-        <TextField id="standard-basic" label="Rating" variant="standard" type="number" step="any" name="rating" value={rating} onChange={(e)=>{setRating(e.target.value)}} />
-        {/* Rating:<input type="number" step="any" name="rating" value={rating} onChange={(e)=>{setRating(e.target.value)}}></input> */}
-        <br></br>
-        <TextField id="standard-basic" label="Summary" variant="standard" type="text" name="summary" value={summary} onChange={(e)=>{setSummary(e.target.value)}} />
-        {/* Summary:<input type="text" name="summary" value={summary} onChange={(e)=>{setSummary(e.target.value)}}></input> */}
-        <br></br>
-        <TextField id="standard-basic" label="PosterLink" variant="standard" type="text" name="poster" value={poster} onChange={(e)=>{setPoster(e.target.value)}}/>
-        {/* PosterLink:<input type="text" name="poster" value={poster} onChange={(e)=>{setPoster(e.target.value)}}></input> */}
-        <br></br>
-        <TextField id="standard-basic" label="TrailerLink" variant="standard" type="text" name="trailer" value={trailer} onChange={(e)=>{setTrailer(e.target.value)}}/>
-        <br></br>
-        {/* <button onClick={addMovie}>Submit</button> */}
-        <br></br>
-        
-        <Button onClick={addMovie} variant="contained">Submit</Button>
-      </form>
+                <Button onClick={addMovie} variant="contained">Submit</Button>
+            </form>
 
-    </div>
-    
+        </div>
 
-  )
+
+    )
 };
 
 
